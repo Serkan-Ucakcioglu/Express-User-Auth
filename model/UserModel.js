2d24d232d87791dd880807dad9ffafa1a2fa1e6a
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-01-10 13:26:37 +0300

    user schema

--- model/UserModel.js
+++ model/UserModel.js
@@ -1,1 +1,23 @@
 const mongoose = require("mongoose");
+
+const validateEmail = function (email) {
+  const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return regex.test(email);
+};
+
+const userSchame = mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    validate: [validateEmail, "Please enter a valid email"],
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("User", userSchame);
