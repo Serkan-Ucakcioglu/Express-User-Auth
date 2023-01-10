065fa5cfe87e2df9c2cef2e833412fa64b2a0bc6
Serkan Uçakcıoğlu <serkanuckc123@gmail.com>
2023-01-10 13:29:19 +0300

    route controllers create

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const app = express();
 const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 const connectDb = require("./config/dbConnect");
+const userRoute = require("./router/UserRoute");
 
 dotenv.config();
 connectDb();
 app.use(cookieParser());
+app.use("/", userRoute);
 
 const port = process.env.PORT || 3001;
 
